66ac963e78b86f2ce95571535d362424c8b54b60
Nzwisisa Chidembo <nzwisisa@gmail.com>
2022-12-26 23:32:56 +0200

    enabled x-ray

--- libs/helpers/sqs_helper.py
+++ libs/helpers/sqs_helper.py
@@ -11,14 +11,14 @@ from libs.helpers.constants_helper import *
 
 import logging
 
-# from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import xray_recorder
 import boto3
 
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
 
 
-# @xray_recorder.capture('sqs_helper_send_sqs_queue_payload')
+@xray_recorder.capture('sqs_helper_send_sqs_queue_payload')
 def send_sqs_queue_payload(queueRegion, queueUrl, queuePayload):
     try:
         client = boto3.client('sqs', region_name=queueRegion)
